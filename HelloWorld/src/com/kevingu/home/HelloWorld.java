8bc0e5139984efe651a6cf9a05580337bf48b693
CAH-KevinGu <kevin.gu@cardinalhealth.com>
2013-11-10 21:59:59 +0800

    Initial the HelloWorld Repository

---
+++ HelloWorld/src/com/kevingu/home/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.kevingu.home;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		i=1;
+		i=2;
+		i=3;
+		i=4;
+		System.out.print("Hello World !!!");
+
+	}
+
+}
