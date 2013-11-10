1bd8961223da0483d8e870028cba3e1363338d20
CAH-KevinGu <kevin.gu@cardinalhealth.com>
2013-11-10 22:57:04 +0800

    v3

--- HelloWorld/src/com/kevingu/home/HelloWorld.java
+++ HelloWorld/src/com/kevingu/home/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	/**
 	 * @param args
 	 */
+	static void PrintSomething(String str) {
+		System.out.print(str);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int i=0;
@@ -13,7 +17,7 @@ public class HelloWorld {
 		i=3;
 		i=4;
 		i=5;
-		System.out.print("Hello World !!! "+i);
+		PrintSomething("Hello World !!! "+i);
 
 	}
 
