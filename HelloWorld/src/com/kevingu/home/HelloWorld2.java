c94baf63dc457298ad6d82247073e453010e38d3
CAH-KevinGu <kevin.gu@cardinalhealth.com>
2014-03-07 15:17:19 +0800

    rename

---
+++ HelloWorld/src/com/kevingu/home/HelloWorld2.java
@@ -0,0 +1,29 @@
+package com.kevingu.home;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	static void PrintSomething(String str) {
+		System.out.print(str);
+		System.out.println(" ----- ");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		i=1;
+		i=2;
+		i=3;
+		i=4;
+		i=5;
+		PrintSomething("Hello World !!! "+i);
+		PrintSomething("Hello again");
+	}
+	
+	static void NewLine() {
+		System.out.println();
+	}
+
+}
