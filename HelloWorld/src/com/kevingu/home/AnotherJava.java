a6a043aa8cab56712ef7caf704ac642ee46cb6e3
CAH-KevinGu <kevin.gu@cardinalhealth.com>
2014-03-10 11:11:00 +0800

    test reset merge

--- HelloWorld/src/com/kevingu/home/AnotherJava.java
+++ HelloWorld/src/com/kevingu/home/AnotherJava.java
@@ -1,18 +1,1 @@
-package com.kevingu.home;
-// dev after master head is reset
-
-public class AnotherJava {
-	void SayHello() {
-		String a ="ahdfdj";
-		// ÖÐÎÄ×¢ÊÍ
-		
-		a = "sad sad sad";
-	}
-	void SayHello_fromDev(){
-//		syso
-		
-	}
-	
-	// before 3rd test force merge
-	// dev before 4th test force merge
-}
+//Dev reset head
