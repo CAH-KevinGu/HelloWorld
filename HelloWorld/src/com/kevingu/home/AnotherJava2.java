ddf8f253b66571842ed87a0a67e491de782c4092
CAH-KevinGu <kevin.gu@cardinalhealth.com>
2014-03-10 11:26:27 +0800

    add 2

---
+++ HelloWorld/src/com/kevingu/home/AnotherJava2.java
@@ -0,0 +1,19 @@
+package com.kevingu.home;
+
+public class AnotherJava {
+	void SayHello() {
+		String a ="ahdfdj";
+		// ÖÐÎÄ×¢ÊÍ
+		
+		a = "sad sad sad";
+	}
+	void SayHello_fromDev(){
+//		syso
+		
+	}
+	
+	// before 3rd test force merge
+	// dev before 4th test force merge
+}
+
+// after reset head
